3c854c23e9996885d6f5122476c8c55c3ff81a62
kareinonitsuke <>
2021-03-01 00:30:45 +0900

    [change] (途中)スロットの結果を絵文字リストの添え字で持つようにする
    当たった時のおめでとうコメント #34　の事前準備

--- src/main.rs
+++ src/main.rs
@@ -79,20 +79,32 @@ async fn is_target_channel(channel_name: String) -> bool{
     return true;
 }
 
-async fn gen_slot_string(slot_row: u8, slot_column: u8) -> String {
+async fn gen_slot_string(slot_row: u8, slot_column: u8) -> (String,bool) {
     let emoji_str_list= vec!["<:momo:747707481282838588>","<:momogang:747708446878728233>"];
     let emoji_length = emoji_str_list.len();
+    //
+    let result_atari = false;
 
+    let mut slot_rand_result: Vec<u8> = Vec::new();
+    for number in 0..slot_row*slot_column {//二次元配列にしようかな
+        let rand = rand::thread_rng().gen_range(0, emoji_length);
+        slot_rand_result.push(rand);
+    }
+    let result_slot_string = gen_string(slot_rand_result,emoji_str_list, slot_row, slot_column).await;
+    return (result_slot_string, result_atari);
+}
+
+async fn gen_string(slot_rand_result: Vec<u8>, emoji_str_list: Vec<&str>, slot_row: u8, slot_column: u8) -> String {
+    let emoji_length = emoji_str_list.len();
     let mut result_slot_string = String::new();
+    //二次元配列にして書き方変えようかなあ
+    //for element in slot_rand_result.iter() {
     for number in 0..slot_row*slot_column {
         //最初も改行入るけど表示されなさそうだしいいかな
         if number%slot_row == 0{
             result_slot_string.push_str("\n");
         }
-        let rand_num = rand::thread_rng().gen_range(0, emoji_length);
-        match rand_num{
-            n=> result_slot_string.push_str(&emoji_str_list[n]),
-        }
+        result_slot_string.push_str(&emoji_str_list[slot_rand_result[number]]);//ここ.get()がよいかも
     }
     return result_slot_string;
 }
@@ -101,8 +113,8 @@ async fn hiyoko_slot(ctx: &Context, msg: &Message,slot_column: u8){
     println!("Shard {}", ctx.shard_id);
     let slot_row = 3;
 
-    let result = gen_slot_string(slot_row, slot_column).await;
-    post_message(&ctx,&msg,result).await;
+    let (result_srting, result_atari) = gen_slot_string(slot_row, slot_column).await;
+    post_message(&ctx,&msg,result_srting).await;
 }
 
 #[tokio::main]
