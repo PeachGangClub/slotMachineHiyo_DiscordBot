03f9ca0f27ae4f618c0ba27ba252459c6ce46286
kareinonitsuke <>
2021-03-19 20:23:32 +0900

    [clean] 関数の役割分担を見直しました。

--- src/hiyokoslot.rs
+++ src/hiyokoslot.rs
@@ -7,41 +7,34 @@ use serenity::{
 
 use crate::common::post;
 
-fn gen_slot_string(slot_row: u8, slot_column: u8) -> (String,bool) {
-    let emoji_str_list= vec!["<:momo:747707481282838588>","<:momogang:747708446878728233>"];
-    let emoji_length = emoji_str_list.len();
-    //
-    let result_atari = false;
-
+fn gen_rand_vec(gen_length: u8, emoji_length:usize) -> (Vec<u8>) {
     let mut slot_rand_result: Vec<u8> = Vec::new();
-    for number in 0..slot_row*slot_column {//二次元配列にしようかな
+    for number in 0..gen_length {
         let rand = rand::thread_rng().gen_range(0, emoji_length);
         let rand = rand as u8;
         slot_rand_result.push(rand);
     }
-    let result_slot_string = gen_string(slot_rand_result,emoji_str_list, slot_row, slot_column);
-    return (result_slot_string, result_atari);
+    return slot_rand_result;
 }
 
-fn gen_string(slot_rand_result: Vec<u8>, emoji_str_list: Vec<&str>, slot_row: u8, slot_column: u8) -> String {
-    let emoji_length = emoji_str_list.len();
-    let mut result_slot_string = String::new();
-    //二次元配列にして書き方変えようかなあ
-    //for element in slot_rand_result.iter() {
+fn gen_string(slot_rand_result: Vec<u8>, emoji_str_list:Vec<&str>, slot_row: u8, slot_column: u8) -> String {
+    let mut result_string = String::new();
     for number in 0..slot_row*slot_column {
         //最初も改行入るけど表示されなさそうだしいいかな
         if number%slot_row == 0{
-            result_slot_string.push_str("\n");
+            result_string.push_str("\n");
         }
-        result_slot_string.push_str(&emoji_str_list[slot_rand_result[number as usize] as usize]);//ここ.get()がよいかも
+        result_string.push_str(&emoji_str_list[slot_rand_result[number as usize] as usize]);//ここ.get()がよいかも
     }
-    return result_slot_string;
+    return result_string;
 }
 
 pub async fn hiyoko_slot(ctx: &Context, msg: &Message,slot_column: u8){
     println!("Shard {}", ctx.shard_id);
     let slot_row = 3;
+    let emoji_str_list= vec!["<:momo:747707481282838588>","<:momogang:747708446878728233>"];
 
-    let (result_srting, result_atari) = gen_slot_string(slot_row, slot_column);
+    let rand_vec = gen_rand_vec(slot_row*slot_column,emoji_str_list.len());
+    let result_srting = gen_string(rand_vec, emoji_str_list, slot_row, slot_column);
     post::post_message(&ctx,&msg,result_srting).await;
 }
