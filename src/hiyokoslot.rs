98ef0d7ddc84584f33e2755f8d78ba5d741d06d8
kareinonitsuke <>
2021-03-24 00:08:37 +0900

    [add] bingo判定(仮)を実装しました

--- src/hiyokoslot.rs
+++ src/hiyokoslot.rs
@@ -43,12 +43,69 @@ pub async fn hiyoko_bingo(ctx: &Context, msg: &Message){
     println!("Shard {}", ctx.shard_id);
     let slot_row = 5;
     let slot_column = 5;
-    let emoji_str_list= vec!["<:bingo_n:823544230546374676>","<:bingo_atari:823544270271021088>","<:bingo_free:823542644062814238>"];
+    let emoji_str_list= vec!["<:bingo_close:823544230546374676>","<:bingo_open:823544270271021088>","<:bingo_atari:823911268729356328>","<:bingo_free:823542644062814238>"];
 
-    let mut rand_vec = gen_rand_vec(slot_row*slot_column,emoji_str_list.len()-1);
-    rand_vec[12] = 2;
-    let result_srting = gen_string(rand_vec, emoji_str_list, slot_row, slot_column);
+    let mut rand_vec = gen_rand_vec(slot_row*slot_column,2);
+    let rand_vec = bingo_check(&mut rand_vec,slot_row,slot_column);
+    let result_srting = gen_string(rand_vec.to_vec(), emoji_str_list, slot_row, slot_column);
     post::post_message(&ctx,&msg,result_srting).await;
+}
+
+fn bingo_check(rand_vec: &mut Vec<u8>, row: u8, column:u8) -> &mut Vec<u8>{
+    //あとで何とかする
+    rand_vec[12] = 3;
+    //縦方向チェック
+    for number in 0..row{
+        //あとで何とかする
+        let is_bingo = isnotzero(vec![rand_vec[(row*0+number) as usize],rand_vec[(row*1+number) as usize],rand_vec[(row*2+number) as usize],rand_vec[(row*3+number) as usize],rand_vec[(row*4+number) as usize]]);
+        if is_bingo == true {
+            println!("tate bingo");
+            //あとで何とかする
+            for n in 0..column{
+                rand_vec[(row*n+number) as usize] = 2;
+            }
+        }
+    }
+    //横方向チェック
+    for number in 0..column{
+        //あとで何とかする
+        //let is_bingo = isnotzero(rand_vec[(((0+row*number) as usize)..((4+row*number) as usize)].to_vec());
+        let is_bingo = isnotzero(vec![rand_vec[(0+row*number) as usize],rand_vec[(1+row*number) as usize],rand_vec[(2+row*number) as usize],rand_vec[(3+row*number) as usize],rand_vec[(4+row*number) as usize]]);
+        if is_bingo == true {
+            println!("yoko bingo");
+            //あとで何とかする
+            for n in 0..row{
+                rand_vec[(n+row*number) as usize] = 2;
+            }
+        }   
+    }
+    //斜め方向チェック
+    //あとで何とかする
+    let is_bingo = isnotzero(vec![rand_vec[0],rand_vec[6],rand_vec[12],rand_vec[18],rand_vec[24]]);
+    if is_bingo == true {
+        println!("naname bingo");
+        //あとで何とかする rowは適切じゃないかも
+        for n in 0..row{
+            rand_vec[(row*n+n) as usize] = 2;
+        }
+    }
+    //あとで何とかする
+    let is_bingo = isnotzero(vec![rand_vec[4],rand_vec[8],rand_vec[12],rand_vec[16],rand_vec[20]]);
+    if is_bingo == true {
+        println!("naname bingo");
+        //あとで何とかする
+        for n in 0..row{
+            rand_vec[(row*n-n) as usize] = 2;
+        }
+    }
+
+    rand_vec[12] = 3;
+    return rand_vec;
+}
+
+fn isnotzero(vec_num: Vec<u8>) -> bool{
+    //ジェネリクスの勉強の後書き換えるとよいかも
+    return vec_num.iter().all(|&x| x > 0);
 }
 
 pub async fn hiyoko_bowling(ctx: &Context, msg: &Message){
